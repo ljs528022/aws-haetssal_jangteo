277f69c7cddf03b53577f8c2bc7b4cabc9f7b5ae
ljs528022 <ljs528022@gmail.com>
2026-02-28 10:36:53 +0900

    site fix

--- src/main/resources/static/js/header/header.js
+++ src/main/resources/static/js/header/header.js
@@ -16,47 +16,11 @@ const headerLayout = document.querySelector(".Header-Layout");
 // 로그인 버튼 클릭 여부
 let isClicked = false;
 
-// 임시 유저 확인용 값
-let userToken = true;
-
 // 토글용 값
 let isActive = false;
 
-// 페이지 로딩 되었을 때 이벤트
-document.addEventListener("DOMContentLoaded", (e) => {
-    if (userToken) {
-        // user 정보가 있을 시, 해당 유저 프로필 사진으로 변경
-        loginButton.classList.add("onlogin");
-        loginBtnContent.innerHTML = `<img src="" alt="profile-img">`;
-
-        // 좋아요 div, 알림 div 표시
-        likeBtn.style.display = "flex";
-        alertBtn.style.display = "flex";
-    } else {
-        // user 정보가 없을 시, 로그인 페이지로 이동,
-        loginButton.classList.remove("onlogin");
-    }
-});
-
 // 로그인 버튼 클릭 시 이벤트
 loginButton.addEventListener("click", (e) => {
-    if (!userToken) {
-        // 로그인 하지 않은 상태일 경우, 로그인 페이지로 이동
-    } else {
-        // 로그인 되어 있을 경우 메뉴바 표시
-        if (!isClicked) {
-            userMenu.style.display = "flex";
-            userMenu.firstElementChild.firstElementChild.innerHTML = `
-            <img src=${""} alt="유저 프로필 이미지" class="MenuList-UserProfile-img">
-            <div class="MenuList-UserProfile-name">
-                <p class="MenuList-UserProfile-p">${"홍길동"}</p>
-            </div>
-            `;
-        } else {
-            userMenu.style.display = "none";
-        }
-    }
-
     // 로그인 버튼 옆 화살표 애니메이션
     if (!isClicked) {
         loginBtnArrow.style.transform = "rotate(180deg)";
